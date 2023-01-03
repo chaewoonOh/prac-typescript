c168e7a9709927b0b34773cf14b4a9b8ce1f99d1
chaewoon-Front <treechae38@naver.com>
2023-01-03 22:25:17 +0900

    예제

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-var 링크 = document.querySelectorAll("naver");
-for (var i = ; i < 3; i++) {
-    var a = 링크[i];
-    if (a instanceof HTMLAnchorElement) {
-        a.href = 'https://kakao.com';
+var Word = /** @class */ (function () {
+    function Word() {
+        var param = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            param[_i] = arguments[_i];
+        }
+        var 숫자들 = [];
+        var 문자들 = [];
+        param.forEach(function (i) {
+            if (typeof i === "string") {
+                문자들.push(i);
+            }
+            else {
+                숫자들.push(i);
+            }
+        });
+        this.num = 숫자들;
+        this.str = 문자들;
     }
-}
+    return Word;
+}());
+var obj = new Word("kim", 3, 5, "park");
+console.log(obj.num);
+console.log(obj.str);
--- index.ts
+++ index.ts
@@ -1,8 +1,23 @@
-let 링크 = document.querySelectorAll("naver");
+class Word {
+  num;
+  str;
 
-for (let i =; i <3; i++){
-  let a = 링크[i];
-  if (a instanceof HTMLAnchorElement) {
-    a.href = 'https://kakao.com'
+  constructor(...param) {
+    let 숫자들: number[] = [];
+    let 문자들: string[] = [];
+
+    param.forEach((i) => {
+      if (typeof i === "string") {
+        문자들.push(i);
+      } else {
+        숫자들.push(i);
+      }
+    });
+    this.num = 숫자들;
+    this.str = 문자들;
   }
 }
+
+let obj = new Word("kim", 3, 5, "park");
+console.log(obj.num);
+console.log(obj.str);
