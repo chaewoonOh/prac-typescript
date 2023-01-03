f44699573ef27ffb56db30b99b6b9d6ed06b1c21
chaewoon-Front <treechae38@naver.com>
2023-01-03 21:01:27 +0900

    예제

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var 이미지 = document.querySelector("#image");
-if (이미지 instanceof HTMLImageElement) {
-    이미지.src = "change.jpg";
+var 링크 = document.querySelectorAll("naver");
+for (var i = ; i < 3; i++) {
+    var a = 링크[i];
+    if (a instanceof HTMLAnchorElement) {
+        a.href = 'https://kakao.com';
+    }
 }
--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
-let 이미지 = document.querySelector("#image");
-if (이미지 instanceof HTMLImageElement) {
-  이미지.src = "change.jpg";
+let 링크 = document.querySelectorAll("naver");
+
+for (let i =; i <3; i++){
+  let a = 링크[i];
+  if (a instanceof HTMLAnchorElement) {
+    a.href = 'https://kakao.com'
+  }
 }
