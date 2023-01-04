4075e355bb72622587d70bd122d122371e27f060
chaewoon-Front <treechae38@naver.com>
2023-01-04 20:43:04 +0900

    narrowing 연습

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
-interface MathObj {
-  plus: (a: number, b: number) => number;
-  minus: (a: number, b: number) => number;
-}
-
-let 오브젝트: MathObj = {
-  plus(a, b) {
-    return a + b;
-  },
-  minus(a, b) {
-    return a - b;
-  },
+type Car = {
+  wheel: "4개";
+  color: string;
 };
+type Bike = {
+  wheel: "2개";
+  color: string;
+};
+
+function 함수(x: Car | Bike) {
+  if (x.wheel === "4개") {
+    console.log("the car is " + x.color);
+  } else {
+    console.log("the bike is " + x.color);
+  }
+}
