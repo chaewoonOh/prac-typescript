a66b6d199bc1371a5b5953493cb28f8756e0e420
chaewoon-Front <treechae38@naver.com>
2023-01-05 21:32:35 +0900

    never 연습

--- index.ts
+++ index.ts
@@ -1,16 +1,7 @@
-type Car = {
-  wheel: "4개";
-  color: string;
-};
-type Bike = {
-  wheel: "2개";
-  color: string;
-};
-
-function 함수(x: Car | Bike) {
-  if (x.wheel === "4개") {
-    console.log("the car is " + x.color);
+function 함수(parameter: string) {
+  if (typeof parameter === "string") {
+    parameter + 1;
   } else {
-    console.log("the bike is " + x.color);
+    parameter;
   }
 }
