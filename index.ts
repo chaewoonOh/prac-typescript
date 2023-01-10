e8a23633bb77c443693ede485d20475e112fff37
chaewoon-Front <treechae38@naver.com>
2023-01-10 20:03:44 +0900

    namespace 연습

--- index.ts
+++ index.ts
@@ -1,23 +1,11 @@
-class Square {
-  constructor(
-    public width: number,
-    public height: number,
-    public color: string
-  ) {}
-  draw() {
-    let a = Math.random();
-    let square = `<div style="position:relative; 
-      top:${a * 400}px; 
-      left:${a * 400}px; 
-      width:${this.width}px; 
-      height : ${this.height}px; 
-      background:${this.color}"></div>`;
-    document.body.insertAdjacentHTML("beforeend", square);
+namespace GoodDog {
+  export type Dog = string;
+}
+namespace BadDog {
+  export interface Dog {
+    name: string;
   }
 }
 
-let 네모 = new Square(30, 30, "red");
-네모.draw();
-네모.draw();
-네모.draw();
-네모.draw();
+let dog1: GoodDog.Dog = "bark";
+let dog2: BadDog.Dog = { name: "paw" };
