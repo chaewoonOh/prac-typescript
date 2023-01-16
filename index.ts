b8d15eaed62d2680368d35711bd5e969b238d880
chaewoon-Front <treechae38@naver.com>
2023-01-16 19:49:58 +0900

    redux type 지정 연습

--- index.ts
+++ index.ts
@@ -1,11 +1,32 @@
-interface CarType {
-  model: string;
-  tax: (price: number) => number;
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+interface Counter {
+  count: number;
 }
 
-class Car implements CarType {
-  model; ///any 타입됨
-  tax(a) {
-    return a * 0.1;
+const 초기값: Counter = { count: 0 };
+
+function reducer(state = 초기값, action: any) {
+  if (action.type === "증가") {
+    return { count: state.count + 1 };
+  } else if (action.type === "감소") {
+    return { count: state.count - 1 };
+  } else {
+    return initialState;
   }
 }
+
+const store = createStore(reducer);
+
+// store의 타입 미리 export 해두기
+export type RootState = ReturnType<typeof store.getState>;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
