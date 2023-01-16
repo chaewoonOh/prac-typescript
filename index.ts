0fbd85887cfd2bc91b6047a58e77ebafe1bdd556
chaewoon-Front <treechae38@naver.com>
2023-01-17 03:59:06 +0900

    index signatures 연습

--- index.ts
+++ index.ts
@@ -1,32 +1,9 @@
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-
-interface Counter {
-  count: number;
+interface StringOnly {
+  [key: string]: string;
 }
 
-const 초기값: Counter = { count: 0 };
-
-function reducer(state = 초기값, action: any) {
-  if (action.type === "증가") {
-    return { count: state.count + 1 };
-  } else if (action.type === "감소") {
-    return { count: state.count - 1 };
-  } else {
-    return initialState;
-  }
-}
-
-const store = createStore(reducer);
-
-// store의 타입 미리 export 해두기
-export type RootState = ReturnType<typeof store.getState>;
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+let obj: StringOnly = {
+  name: "kim",
+  age: "20",
+  location: "seoul",
+};
