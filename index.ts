db3069e3f7414b64256db8cd51ae0de1bc46cdc1
chaewoon-Front <treechae38@naver.com>
2023-01-19 19:48:46 +0900

    object 연습

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-interface StringOnly {
-  [key: string]: string;
-}
-
-let obj: StringOnly = {
-  name: "kim",
-  age: "20",
-  location: "seoul",
+type Bus = {
+  color: string;
+  model: boolean;
+  price: number;
 };
+
+type TypeChanger<MyType, T> = {
+  [key in keyof MyType]: T;
+};
+
+type NewBus = TypeChanger<Bus, boolean>;
+type NewBus2 = TypeChanger<Bus, string[]>;
