0878015c97b6ea12d8081d6c805796a4af9cbf92
chaewoon-Front <treechae38@naver.com>
2023-02-20 19:19:15 +0900

    implements 연습

--- index.ts
+++ index.ts
@@ -1,4 +1,12 @@
-index.ts;
+interface CarType {
+  model: string;
+  tax: (price: number) => number;
+}
 
-declare let a: number;
-console.log(a + 1);
+class Car implements CarType {
+  model; ///any 타입됨
+  tax(a) {
+    ///a 파라미터는 any 타입됨
+    return a * 0.1;
+  }
+}
